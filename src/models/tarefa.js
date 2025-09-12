3569d080acff5a3cfe6ba9bf230e424c49448702
camisbersann <camila.bersan@aluno.senai.br>
2025-09-12 12:46:24 -0300

    feat: comments

--- src/models/tarefa.js
+++ src/models/tarefa.js
@@ -1,6 +1,10 @@
+// Importa a função 'format' da biblioteca 'date-fns' para formatar datas
 import { format } from 'date-fns';
 
+// Declaração de uma classe chamada 'TarefaClass'
 export default class TarefaClass {
+
+     // Método construtor
     constructor(titulo, descricao, data, prioridade) {
         this.titulo = titulo;
         this.descricao = descricao;
@@ -9,10 +13,12 @@ export default class TarefaClass {
         this.status = "pendente";
     }
 
+    // Método para gerar um ID aleatório
     geraId(){
         return Math.floor(Math.random()*1000);
     };
 
+     // Método para retornar a data formatada da tarefa
     getData(){
         if(!this.data){
             return "Data não informada";
@@ -26,10 +32,12 @@ export default class TarefaClass {
         return format(new Date(this.data), "dd/MM/yyyy");
     }
 
+     // Método para marcar a tarefa como concluída
     concluirTafera(){
         this.status = "concluida";
     }
 
+     // Método para reabrir a tarefa (voltar ao status pendente)
     reabrirTarefa(){
         this.status = "pendente";
     }

cef7b038a1a93a2404a7aec9c3b99645eaacb071
camisbersann <camila.bersan@aluno.senai.br>
2025-09-12 13:48:21 -0300

    feat: fix errors and create routes

--- src/context/TaskContext.js
+++ src/context/TaskContext.js
@@ -1,15 +1,15 @@
 import { createContext, useState } from 'react'; // Importa createContext e useState do React
 import TarefaList from '../models/tarefaList'; // Importa a classe TarefaList que gerencia a lista de tarefas
 
-//// Cria o contexto que será compartilhado entre componentes
+// Cria o contexto que será compartilhado entre componentes
 export const TaskContext = createContext();
 
 // Componente provedor do contexto (Provider)
 export const TaskProvider = ({children}) => {
     const [tarefasList] = useState(new TarefaList()); // Cria o estado para a lista de tarefas, usando a classe TarefaList
-    const [setAtualizar] = useState(0);   // Cria um estado para forçar atualização do componente
+    const [atualizarConatdor, setAtualizar] = useState(0);   // Cria um estado para forçar atualização do componente
 
-    const atualizar = () => setAtualizar(setAtualizar + 1);  // Função para atualizar o estado e forçar re-render
+    const atualizar = () => setAtualizar(prev => prev + 1);  // Função para atualizar o estado e forçar re-render
 
     // Função para adicionar uma tarefa
     const addTarefa = (titulo, descricao, data, prioridade) => {

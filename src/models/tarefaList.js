c668a0f63273c3c5771c7cf68d73d5b6d7518116
camisbersann <camila.bersan@aluno.senai.br>
2025-09-12 12:56:07 -0300

    feat: comments

--- src/models/tarefaList.js
+++ src/models/tarefaList.js
@@ -1,35 +1,44 @@
+// Declaração de uma classe chamada 'TarefaList'
 export default class TarefaList{
     constructor(){
-        this.tarefas = [];
+        this.tarefas = []; //Array que vai armazenar os objetos de tarefas
     }
 
+     // Adiciona uma nova tarefa à lista
     addTarefa(tarefa){
         this.tarefas.push(tarefa);
     }
 
+     // Retorna a tarefa pelo ID
     getTarefaById(id){
         return this.tarefas.find(tarefa => tarefa.id == id);
     }
 
+     // Retorna a tarefa pelo status
     getTarefaByStatus(status){
         return this.tarefas.filter(tarefa => tarefa.status == status);
     }
 
+     // Remove a tarefa pelo ID
     removeTarefa(id){
         this.tarefas = this.tarefas.filter(tarefa => tarefa.id != id);
     }
 
+     // Retorna todos os objetos de tarefas
     getAll(){
         return this.tarefas;
     }
 
+   // Alterna o status da tarefa entre "pendente" e "concluída"
     toggleStatus(id){
         const tarefa = this.getTarefaById(id);
         if(tarefa){
             tarefa.status = tarefa.status == "pendente" ? "concluida" : "pendente";
         }
     }
+    
 
+    // Atualiza a tarefa com os novos dados
     updateTarefa(id, titulo, descricao, data, prioridade){
         const tarefa = this.getTarefaById(id);
 

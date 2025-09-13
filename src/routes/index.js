5235e4c9d66e10682683cf2265942ee813113e1f
camisbersann <camila.bersan@aluno.senai.br>
2025-09-12 21:46:04 -0300

    feat: comments

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 
+// Importa as rotas em stack (pilha) definidas em outro arquivo
 import StackRoutes from "./stack.routes";
 
+//Envolve todas as rotas
 export default function Routes() {
     return (
         <NavigationContainer>
